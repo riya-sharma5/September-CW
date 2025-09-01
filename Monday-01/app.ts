02295cb8d55eeed31d9c0241b3a3c42e32d85415
Riya Sharma <riyasharma978715@gmail.com>
2025-09-01 18:37:15 +0530

    implemented

---
+++ Monday-01/app.ts
@@ -0,0 +1,90 @@
+// let arr = [1,2,3, "riya"];
+// // let arr2: number[] = [1, 2,3,4];
+// let arr:[number, string] = [12, "riya"];
+
+
+
+// enum UserRoles{
+//     ADMIN = "admin",
+//     GUEST = "guest",
+//     SUPER_ADMIN = "superadmin"
+// }
+
+//  let a:number;
+// a= 12;
+
+// let a: unknown;
+// a = 12; 
+// a = "riya";
+
+// function abcd(): void{
+//     console.log("hey");
+// }
+
+// let a = null;
+// let a:null = null;
+
+// let a: null;
+// a =12; no
+
+// let a : string | null;
+// a = "riya";
+//  a = 
+
+// let a:undefined;
+
+// function abcd(){
+//     while(true){};
+// }
+
+// interface User{
+//     name: string,
+//     email: string,
+//     password: string
+// }
+
+// function getDataOfUser(obj: User){
+     
+// }
+// getDataOfUser({name:"riya", email: "riya@gmail.com", password: "riya"});
+
+
+// interface User{
+//     name: string,
+//     email: string,
+//     password: string
+        //   gender?: string
+// };
+ 
+// const obj = {
+//     name: "riya",
+//     email: "riya@gmail.com",
+//     password: "abcd"
+// }
+// /////////////
+
+// interface User{
+//    name: string,
+//    email: string,
+//    password: string
+// }
+
+
+// interface Admin extends User{
+//   admin:boolean;
+// }
+
+// function abcd(obj: Admin){
+    
+// }
+
+// type sankhya = number;
+// let a : sankhya;
+
+// type value = string | number | null;
+
+// let a: value;
+
+// function abcd(obj: value){
+
+// }
