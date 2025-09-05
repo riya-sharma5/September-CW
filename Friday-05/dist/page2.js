a1fd29e283787ca52ec5e63c77f65fcda6b1822b
Riya Sharma <riyasharma978715@gmail.com>
2025-09-05 18:32:44 +0530

    implemented

---
+++ Friday-05/dist/page2.js
@@ -0,0 +1,36 @@
+import * as http from 'http';
+
+
+const server = http.createServer((request, response) => {
+    const { url, method } = request;
+
+    response.setHeader('Content-Type', 'text/plain');
+
+    if (method === 'GET') {
+        switch (url) {
+            case '/':
+                response.statusCode = 200;
+                response.write('Welcome to the homepage!');
+                break;
+
+            case '/aboutMe':
+                response.statusCode = 200;
+                response.write('Welcome to the about section');
+                break;
+
+            default:
+                response.statusCode = 404;
+                response.write('404 Not Found');
+                break;
+        }
+    } else {
+        response.statusCode = 405;
+        response.write('Method Not Allowed');
+    }
+
+    response.end();
+});
+
+server.listen(3000);
+console.log('Server started');
+//# sourceMappingURL=server.js.map
---
+++ Friday-05/src/page2.ts
@@ -0,0 +1,35 @@
+import * as http from 'http';
+import type { IncomingMessage, ServerResponse } from 'http';
+
+const server = http.createServer((request: IncomingMessage, response: ServerResponse) => {
+    const { url, method } = request;
+
+    response.setHeader('Content-Type', 'text/plain');
+
+    if (method === 'GET') {
+        switch (url) {
+            case '/':
+                response.statusCode = 200;
+                response.write('Welcome to the homepage!');
+                break;
+
+            case '/aboutMe':
+                response.statusCode = 200;
+                response.write('Welcome to the about section');
+                break;
+
+            default:
+                response.statusCode = 404;
+                response.write('404 Not Found');
+                break;
+        }
+    } else {
+        response.statusCode = 405;
+        response.write('Method Not Allowed');
+    }
+
+    response.end();
+});
+
+server.listen(3000);
+console.log('Server started');
