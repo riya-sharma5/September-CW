ea6993ad3493d462f80ce77f8e76f88e3da784ab
Riya Sharma <riyasharma978715@gmail.com>
2025-09-05 11:52:52 +0530

    implemented

---
+++ Thursday-04/file.js
@@ -0,0 +1,29 @@
+//  practice code 
+
+// myPromise.then(
+//   function(value) { /* code if successful */ },
+//   function(error) { /* code if some error */ }
+// );
+
+
+// async function myFunction() {
+//   return "Hello";
+// }
+// myFunction().then(
+//   function(value) {myDisplayer(value);},
+//   function(error) {myDisplayer(error);}
+// );
+
+
+// async function myFunction() {
+//   return "Hello";
+// }
+
+
+// const getData = async () => {
+//     let y = await "Hello World";
+//     console.log(y);
+// }
+
+// getData();
+
